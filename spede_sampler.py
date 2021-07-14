e5cab91496bc1e1512a1db63e930b2624b34cd4e
Clarke <vsteenderen@gmail.com>
2021-07-14 16:06:55 +0200

    Update spede_sampler.py

--- spede_sampler.py
+++ spede_sampler.py
@@ -56,7 +56,7 @@ menu_def = [
 
 layout_main = [
     [sg.Menu(menu_def, tearoff=False, background_color="lightgreen")],
-    [sg.Image('spede_sampler_image.png')]
+    [sg.Image('spede_sampler_logo.png')]
 ]
 
 ######################################################################################################################
